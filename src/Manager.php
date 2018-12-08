0644c0ad623c8aa9a1aebc94d861802aeec17574
Łukasz Sitnicki <lukasz.sitnicki@gmail.com>
2018-12-08 14:48:28 +0100

    Anotation added to Manager

--- src/Manager.php
+++ src/Manager.php
@@ -10,12 +10,13 @@ use Mrluke\Settings\Contracts\Bag;
  * Manager is main class providing settings management system.
  *
  * @author    Łukasz Sitnicki (mr-luke)
- *
  * @link      http://github.com/mr-luke/settings-manager
  *
  * @version   1.0
- *
  * @license   MIT
+ *
+ * @method    mixed get($key)
+ * @method    mixed set($key, $value)
  */
 class Manager
 {
